3923f92517a789373da1cdb076a1d4df62ca3610
Anubhav Anand <anubhavanand884@gmail.com>
2026-01-05 21:59:35 +0530

    Removed secret

--- DotNet MCP Server/Services/Saudia/SaudiaService.cs
+++ DotNet MCP Server/Services/Saudia/SaudiaService.cs
@@ -12,7 +12,7 @@ public static class SaudiaConstants{
     public const string host = "http://dapi-uat.dcloud.saudia.com";
 
     public const string ClientId = "ddb05723-34db-40d1-8236-31f7379ef537";
-    public const string ClientSecret = "x6X8Q~IiESmxPW9VYCQj0j5cFG4NvNk1Khvi0cM2";
+    public const string ClientSecret = "ddb05723-34db-40d1-8236-31f7379ef537";
     public const string Scope = "259a88f9-c8c4-4083-9a20-a80d018987d7/.default";
     public const string Fact = "{\"keyValuePairs\":[{\"key\":\"flow\",\"value\":\"REVENUE\"},{\"key\":\"market\",\"value\":\"IND\"},{\"key\":\"originCity\",\"value\":\"JED\"},{\"key\":\"channel\",\"value\":\"DESKTOP\"}]}";
 
